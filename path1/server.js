728ba48e7ffe54951e61e40c2d56102722b151e2
Ahmed Shendy <ahmed.shendi@gmail.com>
2021-11-23 10:23:02 +0200

    fix

--- path1/server.js
+++ path1/server.js
@@ -4,7 +4,11 @@ var express = require('express');
 app = express();
 
 app.get('/path1/', function (req, res) {
-  res.send('Hello World from path 1 from pod: ' + process.environment.HOSTNAME + '\n')
+  res.send('Hello World from path1 from service path1: \n')
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World from root path from service path1: \n')
 });
 
 app.listen(8080, function () {
--- path2/server.js
+++ path2/server.js
@@ -4,9 +4,12 @@ var express = require('express');
 app = express();
 
 app.get('/path2/', function (req, res) {
-  res.send('Hello World from path 2 from pod: ' + process.environment.HOSTNAME + '\n')
+  res.send('Hello World from path2 from service path2: \n')
 });
 
+app.get('/', function (req, res) {
+  res.send('Hello World from root path from service path2: \n')
+});
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
